14e13bbc0babe715748dc53a01e59078da4d8372
jeremyholbrecht <Griffley@proton.me>
2024-03-06 10:44:04 +0100

    print rows from books table

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "books"
+)
+
+type Book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
+}
 
 func main() {
 	fmt.Println("Weclome to the bookstore :)")
+
+	//connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	//open db
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//important that the defer statement should come after you check for errors otherwise if db.Query() returns an error
+	//you'll get a panic trying to close a nil resultset
+	defer rows.Close()
+
+	bks := make([]*Book, 0)
+	for rows.Next() {
+		bk := new(Book)
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, bk := range bks {
+		fmt.Printf("%s, %s, %s,€%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
+
 }
